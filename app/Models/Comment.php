84707ffbad96928a09a75916d6622bfda36a96f0
MohanadGhazi123 <muhanad.ghazi16@gmail.com>
2023-12-15 10:17:10 +0000

    Last commit

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -14,4 +14,8 @@ class Comment extends Model
     public function post(){
         return $this->belongsTo(Post::class);
     }
+    public function tags()
+    {
+        return $this->morphToMany(Tag::class, 'taggable');
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
-{
+{    
     use HasFactory;
 
     protected $fillable = ['text', 'posts_image'];
@@ -14,7 +14,14 @@ class Post extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+    public function tags()
+    {
+        return $this->morphToMany(Tag::class, 'taggable');
+
+    }
     public function comments(){
         return $this->hasMany(Comment::class);
     }
+
+
 }
